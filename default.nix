4ca21dca04292b490226d76328336593df510b98
Dustin Lacewell <dlacewell@gmail.com>
2017-10-17 15:46:14 -0500

    Initial commit

---
+++ default.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.programs.emacs.orgExport;
+  emacs = (pkgsemacsPackagesNgGen emacs).emacsWithPackages (epkgs: [ epkgs.htmlize ]);
+  env = { buildInputs = [ pkgs.git pkgs.emacs ]; };
+  script = ''
+    ln -s "${cfg.source}" ./init.org;
+    emacs -Q --script ${./org-export.el} -f export-init-to-html;
+    mv init.html index.html
+    git init;
+    git checkout -b gh-pages
+    git remote add origin "${cfg.giturl}"
+    git add index.html;
+    git commit -m "autodeploy";
+    git push --force origin gh-pages;
+    cp ./index.html $out;
+  '';
+  result = pkgs.runCommand "exportOrg" env script;
+
+in {
+
+  options.programs.emacs.orgExport = {
+    enable = mkEnableOption "Orgfile Github-Pages Export";
+    source = mkOption {
+      type = types.path;
+      description = ''
+        The source orgfile to export to Github-Pages.
+      '';
+    };
+    giturl = mkOption {
+      type = types.str;
+      description = ''
+        The Git URL of the Github-Pages repository.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.file.".emacs.d/init.html".source = result;
+  };
+}
