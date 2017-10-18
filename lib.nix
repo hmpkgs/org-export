a7b0626b3df980f0c73509a0b25e7e5b18dee10c
Dustin Lacewell <dlacewell@gmail.com>
2017-10-17 23:45:32 -0500

    Add experimental library module

---
+++ lib.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+let
+  emacs = (pkgsemacsPackagesNgGen pkgs.emacs).emacsWithPackages (epkgs: [ epkgs.htmlize ]);
+
+{
+  orgExport = { infile, giturl }:
+  let
+      inpath = toPath infile;
+      env = { buildInputs = [ emacs pkgs.git ]; };
+      script = ''
+        ln -s "${infile}" ./init.org;
+        emacs -Q --script ${./org-export.el} -f export-init-to-html;
+        mv init.html index.html
+        git init;
+        git checkout -b gh-pages
+        git remote add origin "${giturl}"
+        git add index.html;
+        git commit -m "autodeploy";
+        git push --force origin gh-pages;
+        cp ./index.html $out;
+      '';
+    in pkgs.runCommand "${infile}-org-export" env script;
+}
